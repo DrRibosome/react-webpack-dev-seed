da2fb483ed2b8049f6984b1853b50229bf94ed53
Jack Crawford <jdc2172@gmail.com>
2016-02-08 16:43:34 -0500

    added bootstrap navbar

--- app/main.js
+++ app/main.js
@@ -1,46 +1,30 @@
 import React from 'react';
-import Hello from './component.jsx';
 import {Navbar, Nav, NavItem, MenuItem, NavDropdown} from 'react-bootstrap'
 
+import AppNav from './nav.component.jsx'
+
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/docs/examples/starter-template.css';
 
 function main() {
-    React.render(<App />, document.getElementById('app'));
+  React.render(<App/>, document.getElementById('app'));
 }
-
-const navbarInstance = (
-  //<h1>Hello world4</h1>
-  <Navbar inverse>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
 
 class App extends React.Component {
   render() {
-      return navbarInstance;
+    let navStyle = {borderRadius:"0px"} // remove rounded edges on navbar
+    return (
+      <div className="app">
+      <Navbar inverse style={navStyle}>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap test</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>
+
+      <h1>title here</h1>
+      </div>
+    );
   }
 }
 
