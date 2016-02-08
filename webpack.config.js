cfc127dfd25bfabf61663e30c9fd245bc58f05f4
Jack Crawford <jdc2172@gmail.com>
2016-02-08 14:58:22 -0500

    basic react setup with jsx es2015 parsing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app/main.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
+  entry: path.resolve(__dirname, 'app/main.js'),
+  output: {
+    path: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? './dist' : './build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/, // regex test js or jsx
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
 };
